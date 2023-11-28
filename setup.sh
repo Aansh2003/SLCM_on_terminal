cfe886de996239f3f37e1d51ff29a9076299e20d
Aansh Basu <96300383+Aansh2003@users.noreply.github.com>
2023-11-28 19:11:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 cd;
-echo "alias slcm-attendance='python3 SLCM_on_terminal/attendance.py'" >> .bashrc;
-echo "alias slcm-internals='python3 SLCM_on_terminal/internals.py'" >> .bashrc;
+echo "alias slcm-attendance='python3 $PWD/attendance.py'" >> .bashrc;
+echo "alias slcm-internals='python3 $PWD/SLCM_on_terminal/internals.py'" >> .bashrc;
